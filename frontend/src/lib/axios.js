cb384bedb7c8ab331810403d9c84d82952fc726c
C123456arlos <118953415+C123456arlos@users.noreply.github.com>
2026-06-15 11:12:09 -0400

    first commit

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -6,3 +6,11 @@ export const axiosInstance = axios.create({
     baseURL: BASE_URL,
     withCredentials: true
 })
+// import axios from 'axios'
+
+// // const BASE_URL = import.meta.MODE === 'development' ? 'http://localhost:5001/api' : '/api'
+
+// export const axiosInstance = axios.create({
+//     baseURL: 'http://localhost:5001/api',
+//     withCredentials: true
+// })
